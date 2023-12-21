7e9e2c78810cfc34b64c037922b732faaa38bef3
beckkh <beck@tsmtech.co.kr>
2023-12-21 18:16:41 +0900

    pure

--- nbs/src/main/java/com/nbs/nbs/entity/logging/ErrorLoggingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nbs.nbs.entity.logging;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ErrorLoggingRepository extends JpaRepository<ErrorLoggingEntity, Long> {
-}
